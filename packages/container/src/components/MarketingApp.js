5c9462c9f2cc64f083739e4fa7855e1bd4c17d2f
Ankit Jaitak <ankitjaitak9833@gmail.com>
2023-06-07 10:54:39 +0530

    container code commited

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,17 @@
+import { mount } from 'marketing/MarketingApp';
+import React , {useRef, useEffect} from 'react';
+console.log(mount);
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+     mount(ref.current);
+    });
+
+    return <div ref = {ref} />;
+}
+
+// We are writing this js file to execute the mount function as it is coming from other micro frontend.
+// Mount function is a function(not React component) that takes reference of an HTML element and displays content inside that element.
+// mount is not a React component that can be directy used as <mount/>
+// We are calling mount function with an element ref from userRef(null)  and it will return us the code from mount funtion and we will render it in a div usinf ref = {ref}
