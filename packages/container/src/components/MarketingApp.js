31e8ccaa6ece4b2337e8f98888627846477c1bf0
Ankit Jaitak <ankitjaitak9833@gmail.com>
2023-06-08 14:08:18 +0530

    added to and fro route change configuration in container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,24 @@
 import { mount } from 'marketing/MarketingApp';
 import React , {useRef, useEffect} from 'react';
-
+import { useHistory } from 'react-router-dom';
 console.log(mount);
 
 export default () => {
     const ref = useRef(null);
-
+    const history = useHistory();
     useEffect(() => {
-      mount(ref.current);
-    });
+     const {onParentNavigate} =  mount(ref.current, {
+        onNavigate : ({pathname : nextPathName}) => {
+            console.log('The container noticed navigation in Marketing',nextPathName);
+            const {pathname} =  history.location;
+
+            if(pathname!==nextPathName){
+                history.push(nextPathName);
+            }
+        }
+      });
+      history.listen(onParentNavigate);
+    }),[];
 
     return <div ref = {ref} />;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// Mount function to start ut the app
 
-const mount  = (el)  => {
+import {createMemoryHistory, createBrowserHistory} from 'history'; //Note: here we are using history instead of react-router-dom
+// as react-router-dom internally uses history
+
+
+// Mount function to start ut the app
+const mount  = (el, {onNavigate, defaultHistory})  => {
+ 
+    const history =  defaultHistory ||  createMemoryHistory(); // if defualtHistory is present use it else create MemoryHistory
+
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    return {
+        onParentNavigate({pathname: nextPathName}) {
+            const {pathname} = history.location;
+
+            if(pathname!==nextPathName) {
+                history.push(nextPathName);
+            }
+        }
+    }
 };
 
 // If we are in development and in isolation, 
@@ -16,7 +38,10 @@ if( process.env.NODE_ENV === 'development' ) {
      const devRoot = document.querySelector('#_marketing-dev-root');
 
      if(devRoot) {
-        mount(devRoot);
+
+        // If app is loaded in development / isolation mode then we want to use browserhistory
+        //as we want to see url getting changes in urlbar
+        mount(devRoot, {defaultHistory: createBrowserHistory()});
      }
 }
 
